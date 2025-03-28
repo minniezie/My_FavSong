fac603d5d28aa77e2fcd928e910faecaad3cf270
minniezie <minni.wadhwa9@gmail.com>
2025-03-28 11:56:11 -0400

    Add files via upload

---
+++ My_Fav.js
@@ -0,0 +1,12 @@
+<body>
+    <h1 id="songTitle">My Favorite Song</h1>
+    <script>
+        document.getElementById("songTitle").onclick = function() {
+            this.style.color = this.style.color === 'red' ? 'navy' : 'red';
+        };
+    </script>
+</body>
+
+
+
+
